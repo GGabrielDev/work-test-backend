0d357f6219482e11f013a9c5e5168c779d8703f4
Luis Gabriel Ramos Robles <luisgabrielramosrobles@gmail.com>
2022-01-18 10:07:39 -0400

    Set up boilerplate, user entity and routes created

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,55 @@
-import {Entity, ObjectIdColumn, ObjectID, Column} from "typeorm";
+import {
+  Entity,
+  ObjectIdColumn,
+  ObjectID,
+  Column,
+  BeforeInsert,
+} from "typeorm";
+import bcrypt = require("bcrypt");
 
-@Entity()
+@Entity("users")
 export class User {
+  @ObjectIdColumn()
+  id: ObjectID;
 
-    @ObjectIdColumn()
-    id: ObjectID;
+  @Column({
+    unique: true,
+  })
+  email: string;
 
-    @Column()
-    firstName: string;
+  @Column()
+  password: string;
 
-    @Column()
-    lastName: string;
+  @BeforeInsert()
+  hashedPassword() {
+    if (!this.password) {
+      return;
+    }
 
-    @Column()
-    age: number;
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err) {
+        return;
+      }
 
+      bcrypt.hash(this.password, salt, (err, hash) => {
+        if (err) {
+          return;
+        }
+        this.password = hash;
+      });
+    });
+  }
+
+  constructor(email: string, password: string) {
+    this.email = email;
+    this.password = password;
+  }
+
+  public comparePassword(candidatePassword: string): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+        return err ? reject(err) : !isMatch ? reject(false) : resolve(true);
+      });
+    });
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,19 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import express = require("express");
+import { createConnection } from "typeorm";
+import authRoutes from "./routes/authRoutes";
 
-createConnection().then(async connection => {
+createConnection()
+  .then(async (connection) => {
+    const app: express.Express = express();
+    app.use(express.json());
+    app.use(authRoutes);
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+    app.listen(3000, () => {
+      console.log("Listening at port 3000");
+    });
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
-
-    console.log("Here you can setup and run express/koa/any other framework.");
-
-}).catch(error => console.log(error));
+    connection.connect();
+    console.log("Connected");
+  })
+  .catch((error) => console.log(error));
