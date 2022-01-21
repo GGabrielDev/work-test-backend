93355261fa4ca1e164dd87865ee16c45ab5bff23
Luis Gabriel Ramos Robles <luisgabrielramosrobles@gmail.com>
2022-01-21 13:33:07 -0400

    Solved a error on DeleteCategory controller

--- src/controller/DeleteCategory.ts
+++ src/controller/DeleteCategory.ts
@@ -1,13 +1,20 @@
 import { Request, Response } from "express";
 import { getMongoRepository } from "typeorm";
 import { Category } from "../entity/Category";
+import { Product } from "../entity/Product";
 
 export async function DeleteCategory(req: Request, res: Response) {
   const manager = getMongoRepository(Category);
   const category = await manager.findOne(req.params.id);
 
   if (category) {
-    if (!category.products || !(category.products.length > 0)) {
+    const productManager = getMongoRepository(Product);
+    const products = await productManager.find();
+    const results = products.filter(
+      (product) => product.category.name == category.name
+    );
+
+    if (!(results.length > 0)) {
       try {
         await manager.delete(category);
         res.status(200).send("Category deleted susccesfully");
